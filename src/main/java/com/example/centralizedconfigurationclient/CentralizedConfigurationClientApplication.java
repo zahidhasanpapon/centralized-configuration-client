283517da32ca430adb8716d08929857a43b3e2e2
Zahid Hasan <pzahidhasan@outlook.com>
2023-07-09 22:34:05 +0600

    initialized centralized config client

---
+++ src/main/java/com/example/centralizedconfigurationclient/CentralizedConfigurationClientApplication.java
@@ -0,0 +1,31 @@
+package com.example.centralizedconfigurationclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class CentralizedConfigurationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizedConfigurationClientApplication.class, args);
+	}
+
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+	@Value("${greeting:Hello default}")
+	private String message;
+
+	@RequestMapping("/message")
+	String getMessage() {
+		return this.message;
+	}
+
+}
